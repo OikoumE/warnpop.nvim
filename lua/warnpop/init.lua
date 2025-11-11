94d7be429b741883ade6c89180c47563c328ade6
OikoumE <oikoume@gmail.com>
2025-11-11 12:04:11 +0100

    added ]e keymap

--- lua/warnpop/init.lua
+++ lua/warnpop/init.lua
@@ -2,7 +2,6 @@ local M = {
   buf = vim.api.nvim_create_buf(false, true), -- listed =false, scratch =true
   win = -1,
   default_win_opts = {
-
     relative = "editor",
     width = 17,
     height = 1,
@@ -15,20 +14,40 @@ local M = {
   nsName = "Warnpop",
   eHl = "ErrorHighlight",
   wHl = "WarnHighlight",
+  last_diag = {},
 }
-
+M.keymap_set = function()
+  print("wtf!!")
+  vim.notify("wtf", vim.log.levels.INFO)
+  vim.keymap.set("n", "]e", M.goto_last_diag, { desc = "goto next [E]rror" })
+  -- vim.keymap.set("n", "]e", M.goto_last_diag, { desc = "goto next [E]rror" })
+  require("which-key").add({ { "]e", M.goto_last_diag, desc = "goto next [E]rror", mode = "n" } })
+end
+M.goto_last_diag = function()
+  local diag = M.last_diag
+  vim.notify("TEEEEEEEEEEEEEEST!", vim.log.levels.WARN)
+  if not diag or not vim.api.nvim_buf_is_valid(diag.diag.buf) then
+    vim.notify("no valid diags stored", vim.log.levels.WARN)
+    return
+  end
+  -- switch buffer
+  vim.api.nvim_set_current_buf(diag.diag.buf)
+  -- jump to line
+  vim.api.nvim_win_set_cursor(0, { diag.diag.lnum + 1, diag.diag.col })
+end
 M.ns = vim.api.nvim_create_namespace(M.nsName)
-vim.api.nvim_set_hl(0, M.eHl, { bg = "#FF0000", fg = "Cyan" })
-vim.api.nvim_set_hl(0, M.wHl, { bg = "#FFFF00", fg = "Blue" })
-M.setup = function(win_opts)
-  vim.api.nvim_buf_set_var(M.buf, "modifiable", false)
-  -- show shit in status line
+M.create_autocmd = function(win_opts)
+  print("wtf!! create")
+  vim.notify("wtf create", vim.log.levels.INFO)
   vim.api.nvim_create_autocmd("DiagnosticChanged", {
     callback = function(args)
       local diag_count = { error = 0, warning = 0 }
       local diagnostics = vim.diagnostic.get(args.buf)
       for i, diag in ipairs(diagnostics) do
         if diag.severity == vim.diagnostic.severity.ERROR then
+          M.last_diag = { diag, args }
+          if vim.api.nvim_buf_is_valid(args.buf) then
+          end
           diag_count.error = diag_count.error + 1
         elseif diag.severity == vim.diagnostic.severity.WARN then
           diag_count.warning = diag_count.warning + 1
@@ -44,15 +63,22 @@ M.setup = function(win_opts)
         vim.api.nvim_buf_set_lines(M.buf, -2, -1, false, { errStr })
         vim.api.nvim_buf_set_extmark(M.buf, M.ns, 0, 2, { end_col = errMid - 1, hl_group = M.eHl })
         vim.api.nvim_buf_set_extmark(M.buf, M.ns, 0, errMid, { end_col = errLen - 2, hl_group = M.wHl })
-
         vim.api.nvim_win_set_width(M.win, errLen)
-        -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#FF0000", fg = "#D8DEE9" }) -- Example: set custom background and foreground
       elseif vim.api.nvim_win_is_valid(M.win) then
         vim.api.nvim_win_close(M.win, true)
         diag_count = { error = 0, warning = 0 }
       end
     end,
   })
+end
+M.setup = function(win_opts)
+  print("wtf!! setup")
+  vim.notify("setupcreate", vim.log.levels.INFO)
+  M.keymap_set()
+  vim.api.nvim_set_hl(0, M.eHl, { bg = "#FF0000", fg = "Cyan" })
+  vim.api.nvim_set_hl(0, M.wHl, { bg = "#FFFF00", fg = "Blue" })
+  vim.bo[M.buf].modifiable = false
+  M.create_autocmd(win_opts)
 end
 
 return M
